fc53fe4bf852de3b1dde04ed5362a449aa3068cf
Samir Aryamane <saryamane>
2015-12-16 09:25:50 -0800

    Modifications to the employment data file

--- scripts/R_Employment_Data_Analysis.R
+++ scripts/R_Employment_Data_Analysis.R
@@ -91,4 +91,171 @@ intersect(names(ann2012_1),names(ownership))
 intersect(names(ann2012_1),names(size))
 # size_code
 
-head(agglevel)
+library(data.table)
+typeof(ann2012_1)
+typeof(ann2012)
+codes <- c('agglevel','industry','ownership','size')
+ann2012full <- ann2012
+for(i in 1:length(codes)){
+  eval(parse(text=paste('ann2012full <- left_join(ann2012full, ',codes[i],')', sep='')))
+}
+
+head(ann2012full)
+
+# Adding the geographical information to the analysis.
+# Let's look at the geo distribution of wages across the US.
+# For this we have to have maps package installed.
+
+# We already have the area dataset imported into R.
+
+head(area)
+
+# We need to capitalize the names per the conventions, so let's write a function to do tht.
+
+simpleCap <- function(x) {
+  if(!is.na(x)){
+    s <- strsplit(x,' ')[[1]]
+    paste(toupper(substring(s,1,1)),substring(s,2), sep='',collapse = ' ')
+  } else {NA}
+}
+
+data("county.fips")
+head(county.fips)
+
+# This fips has missing leading 0's there, so let's use the stringr package to add that.
+
+county.fips$fips <- str_pad(county.fips$fips, width = 5, pad="0")
+
+# Now we have padded the left with 0's to match our dataset fips value.
+
+# Next we want to separate out the county names from the polynames in the column of polyname
+
+county.fips$polyname <- as.character(county.fips$polyname)
+county.fips$polyname <- sapply(
+  gsub('[a-z\ ]+,([a-z\ ]+)','\\1',county.fips$polyname),simpleCap)
+
+head(county.fips$polyname)
+county.fips <- unique(county.fips)
+nrow(county.fips)
+
+# Let's do something similar for the state.fips table.
+
+data('state.fips')
+head(state.fips)
+
+# Again pad the fips column with 0's 
+
+state.fips$fips <- str_pad(state.fips$fips, width=2, pad="0", side='left')
+state.fips$state <- as.character(state.fips$polyname)
+state.fips$state <- gsub("([a-z\ ]+):[a-z\ \\']+",'\\1',state.fips$state)
+state.fips$state <- sapply(state.fips$state, simpleCap)
+mystatefips <- unique(state.fips[,c('fips','abb','state')])
+nrow(mystatefips)
+
+# The setdiff set operation looks for all the elements in the first set that are not in the second set.
+
+lower48 <- setdiff(unique(state.fips$state), c('Hawaii','Alaska'))
+
+# Finally we put all this information we processed into a single dataset.
+myarea <- NULL # This resets the variable in case you make a mistake.
+myarea <- merge(area, county.fips, by.x='area_fips', by.y='fips', all.x=TRUE)
+head(myarea)
+myarea$state_fips <- substr(myarea$area_fips,1,2)
+myarea <- merge(myarea, mystatefips, by.x='state_fips', by.y='fips', all.x=TRUE)
+
+# Lastly we join this geo info with our dataset, and filter it to keep only the 48 states data
+
+ann2012full <- left_join(ann2012full, myarea)
+head(ann2012full)
+ann2012full <- filter(ann2012full, state%in% lower48)
+
+# Next lets store this final dataset in R data (rda) file on disk.
+# This provides an efficient storage mechanism for R objects on the disk.
+
+save(ann2012full, file='../../Practical_DataScienceTools_Cookbook_recipes/data/ann2012full.rda', compress=TRUE)
+
+# Now we have all the data we need for our analysis. A peek at the metadata tells us that to get the data
+# in the state format, we need to use the filter agglvl_code of 50.
+
+d.state <- filter(ann2012full, agglvl_code==50)
+head(d.state)
+
+d.state <- select(d.state, state, avg_annual_pay, annual_avg_emplvl)
+
+# We then create two new variables, wage and empquantile for our statistical needs, where we discretize our 
+# continous variables.
+
+d.state$wage <- cut(d.state$avg_annual_pay,
+                    quantile(d.state$avg_annual_pay, c(seq(0,.8, by=.2), .9,.95,.99, 1)))
+
+d.state$empquantile <- cut(d.state$annual_avg_emplvl, 
+                           quantile(d.state$annual_avg_emplvl, c(seq(0,.8,by=.2),.9,.95,.99,1)))
+
+# We want the output of these discrete variables to be useful, so we issue the following commands to 
+# harmonize those derived fields.
+
+x <- quantile(d.state$avg_annual_pay, c(seq(0,.8,by=.2),.9, .95, .99, 1))
+x
+xx <- paste(round(x/1000),'K', sep='') # Convert the large numbers into thousands buckets
+xx
+Labs <- paste(xx[-length(xx)],xx[-1],sep='-') # This helps us to create a range.
+Labs
+levels(d.state$wage) <- Labs
+head(d.state)
+
+# This is how we do bucketing in R, very slick and clean. Definitely recommend over the MySQL way of doing things.
+
+# Next we do the same for the employee quantile group.
+
+x <- quantile(d.state$annual_avg_emplvl, c(seq(0,.8,by=.2),.9,.95,.99,1))
+x
+xx <- paste(round(x/1000),'K',sep='')
+xx
+Labs <- paste(xx[-length(xx)],xx[-1], sep='-')
+Labs
+levels(d.state$empquantile) <- Labs
+
+head(d.state)
+
+# We now perform the same steps for the city information where the code==70.
+
+d.cty <- filter(ann2012full, agglvl_code==70)
+d.cty <- select(d.cty, state, abb, avg_annual_pay, annual_avg_emplvl)
+names(d.cty)
+
+d.cty$wage <- cut(d.cty$avg_annual_pay,
+                    quantile(d.cty$avg_annual_pay, c(seq(0,.8, by=.2), .9,.95,.99, 1)))
+
+d.cty$empquantile <- cut(d.cty$annual_avg_emplvl, 
+                           quantile(d.cty$annual_avg_emplvl, c(seq(0,.8,by=.2),.9,.95,.99,1)))
+
+x <- quantile(d.cty$avg_annual_pay, c(seq(0,.8,by=.2),.9, .95, .99, 1))
+x
+xx <- paste(round(x/1000),'K', sep='') # Convert the large numbers into thousands buckets
+xx
+Labs <- paste(xx[-length(xx)],xx[-1],sep='-') # This helps us to create a range.
+Labs
+levels(d.cty$wage) <- Labs
+head(d.cty)
+
+# Now we do the same for the empl salary level on the city level.
+
+x <- quantile(d.cty$annual_avg_emplvl, c(seq(0,.8,by=.2),.9,.95,.99,1))
+x
+xx <- paste(round(x/1000),'K',sep='')
+xx
+Labs <- paste(xx[-length(xx)],xx[-1], sep='-')
+Labs
+levels(d.cty$empquantile) <- Labs
+
+head(d.cty)
+
+# Now lets work on drawing out the map choropleth image using ggplot2
+
+state_df <- map_data('state')
+county_df <- map_data('county')
+
+head(state_df)
+head(county_df)
+
+
